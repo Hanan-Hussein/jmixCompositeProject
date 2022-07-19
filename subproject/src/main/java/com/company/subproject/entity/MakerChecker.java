4e0a202a7b17604017a196fe5f7799374ba85ef7
Hanan-Hussein <hanhuss035@gmail.com>
2022-07-19 15:41:49 +0300

    Untracked files

---
+++ subproject/src/main/java/com/company/subproject/entity/MakerChecker.java
@@ -0,0 +1,167 @@
+package com.company.subproject.entity;
+
+import io.jmix.core.annotation.DeletedBy;
+import io.jmix.core.annotation.DeletedDate;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@JmixEntity
+@MappedSuperclass
+public class MakerChecker {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "UPDATE_DATA")
+    private String updateData;
+
+    @Column(name = "MESSAGE")
+    private String message;
+
+    @Column(name = "APPROVAL_STATUS")
+    private String approvalStatus;
+
+    @Column(name = "ACTION_")
+    private String action;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @CreatedBy
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "CREATED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @LastModifiedBy
+    @Column(name = "LAST_MODIFIED_BY")
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @DeletedBy
+    @Column(name = "DELETED_BY")
+    private String deletedBy;
+
+    @DeletedDate
+    @Column(name = "DELETED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deletedDate;
+
+    public ActionStatus getAction() {
+        return action == null ? null : ActionStatus.fromId(action);
+    }
+
+    public void setAction(ActionStatus action) {
+        this.action = action == null ? null : action.getId();
+    }
+
+    public ApproveStatus getApprovalStatus() {
+        return approvalStatus == null ? null : ApproveStatus.fromId(approvalStatus);
+    }
+
+    public void setApprovalStatus(ApproveStatus approvalStatus) {
+        this.approvalStatus = approvalStatus == null ? null : approvalStatus.getId();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUpdateData() {
+        return updateData;
+    }
+
+    public void setUpdateData(String updateData) {
+        this.updateData = updateData;
+    }
+
+    public Date getDeletedDate() {
+        return deletedDate;
+    }
+
+    public void setDeletedDate(Date deletedDate) {
+        this.deletedDate = deletedDate;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    @PrePersist
+    public void prePersist() {
+        this.setApprovalStatus(ApproveStatus.NOT_APPROVED);
+        this.setAction(ActionStatus.CREATION);
+    }
+}
